ef8a63c0999f2b137cf690337b5a6a3fe80b1dd8
Jonathan <>
2026-09-11 00:04:17 +0800

    add provider and variables

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+    type = string
+    description = "The name of the project"
+    default = "aws-defence-in-depth-incident-rebuild"
+}
+
+variable "aws_region" {
+    type = string
+    description = "The region of the project"
+    default = "ap-southeast-1"
+}
